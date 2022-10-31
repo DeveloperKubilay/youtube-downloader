aa1ba36e0f401eb12884995c1e1882e2d8e6adf2
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2022-11-01 00:44:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ ffmpeg.setFfmpegPath(require('@ffmpeg-installer/ffmpeg').path);
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 rl.question("Video ise 1'i Müzikse 2'yi tuşlayınız: ", function (tür) {
     rl.question('Video linki: ', function (url) {
-        video = ytdl(url, { quality: 'highestaudio' })
-        video.on('info', (info) =>{ 
-                console.log(`İndiriliyor bittiğinde otomatik kapanıcaktır`);
+        rl.question('Dosya konumu girin: ', function (yer) {
+            if(!tür || !url || !yer) return console.log("Bazı yerleri unutun galiba");
+            video = ytdl(url, { quality: 'highestaudio' })
+            video.on('info', (info) => {
+                console.log(`İndiriliyor`);
                 if (tür === "1") {
-                    yer = "C:\\Users\\kubil\\Desktop\\edit\\fidyo"
                     video.pipe(fs.createWriteStream(path.join(yer, latinize(info.videoDetails.title) + '.mp4')))
                     video.on('end', function (info) { process.exit(0) })
                 } else {
-                    yer = "C:\\Users\\kubil\\Desktop\\edit\\müzik"
                     ffmpeg(video).audioBitrate(128).save(path.join(yer, latinize(info.videoDetails.title) + '.mp3')).on('progress', p => {
                         readline.cursorTo(process.stdout, 0);
                         process.stdout.write(`${p.targetSize}kb indirildi ${p.currentKbps}kb/s ${p.timemark}`)
                     }).on('end', () => { process.exit(0) });
                 } rl.close();
             })
+        })
     })
 });
